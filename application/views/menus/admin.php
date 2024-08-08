a4b60e43da14442d0a6625a155b6db080c6ec559
fercho <daza.fernando.940@gmail.com>
2024-08-08 01:10:54 -0400

    Logo de la empresa y cargado de la vista de admin

---
+++ application/views/menus/admin.php
@@ -0,0 +1,27 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Menú</h1>
+        </div>
+    </div>
+    <!-- End Page Content -->
+    <div class="card shadow mb-4 border-left-orange">
+        <div class="card-header py-3">
+            <h4 class="m-0 font-weight-bold text-dark"><?PHP ECHO $respuesta; ?></h4>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm-7">
+                    <img  width="650" height="400" src="<?PHP ECHO BASE_URL(); ?>Template/images/FINSTER SYSTEMS.png">
+                </div>
+                <div class="col-sm-4">
+                    <h5>- Somos </h5>
+                    <hr>
+                    <h5>- Contamos</h5>
+                    <hr>
+                    <h5>- Como </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
